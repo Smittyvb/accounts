a7ba871f4e266bcede9b2b54081fceb17319172c
Sebastian <sebastian@nimiq.com>
2018-12-19 14:15:27 +0100

    Fix endless-loop request for change-passphrase

--- src/views/ChangePassphrase.vue
+++ src/views/ChangePassphrase.vue
@@ -5,13 +5,16 @@ import { Component, Vue } from 'vue-property-decorator';
 import { ParsedChangePassphraseRequest } from '../lib/RequestTypes';
 import { SimpleRequest } from '@nimiq/keyguard-client';
 import { WalletStore } from '@/lib/WalletStore';
+import { State } from 'vuex-class';
 import { Static } from '../lib/StaticStore';
 
 @Component
 export default class ChangePassphrase extends Vue {
     @Static private request!: ParsedChangePassphraseRequest;
+    @State private keyguardResult!: KeyguardRequest.SimpleResult;
 
     public async created() {
+        if (this.keyguardResult) return;
         const wallet = await WalletStore.Instance.get(this.request.walletId);
         if (!wallet) throw new Error('Wallet ID not found');
 
